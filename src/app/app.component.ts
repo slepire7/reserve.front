f2b4dd2b44994eb9554aa1ac499e85db032b3b28
Joao Reis <joao.reis@targetwork.com.br>
2021-08-19 10:30:29 -0400

    Dados Iniciais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Countrie } from './model/countries';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  _urlBase: string = 'http://localhost:5000/'
   title = 'covidapp';
+  TopTenCountries: Countrie;
+  tableTotalConfirmed: boolean = false;
+  constructor(public http: HttpClient) {
+    this.TopTenCountries = {}
+  }
+
+
+  TakeCountries() {
+    this.http.get<any>(`${this._urlBase}DadosIniciais`).subscribe(res => {
+      this.TopTenCountries = res
+      console.info("Top 10",this.TopTenCountries)
+    }, erro => {
+      alert('Falha')
+    })
+  }
+
+
 }
